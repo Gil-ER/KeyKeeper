557f29c3cb480f22feecfe839a394535ee311527
Gil-ER <90463047+Gil-ER@users.noreply.github.com>
2022-09-28 16:27:49 -0400

    Update KeyKeeper.lua

--- KeyKeeper/KeyKeeper.lua
+++ KeyKeeper/KeyKeeper.lua
@@ -87,10 +87,17 @@ end
 
 --event frame
 local frame = CreateFrame("FRAME");
+frame:RegisterEvent("CHALLENGE_MODE_MEMBER_INFO_UPDATED");
+
+
+--Run these once then unregister
 frame:RegisterEvent("ZONE_CHANGED");
-frame:RegisterEvent("BAG_UPDATE");
 frame:RegisterEvent("PLAYER_ENTERING_WORLD");
+--Looking form new keys
+frame:RegisterEvent("BAG_UPDATE");
+--addon messages
 frame:RegisterEvent("CHAT_MSG_ADDON");
+--For linked keys
 frame:RegisterEvent("CHAT_MSG_CHANNEL");
 frame:RegisterEvent("CHAT_MSG_GUILD");
 frame:RegisterEvent("CHAT_MSG_OFFICER");
@@ -100,6 +107,101 @@ frame:RegisterEvent("CHAT_MSG_WHISPER");
 local refreshFlag = true;
 
 function frame:OnEvent(event, ...)
+	if event == "CHALLENGE_MODE_MEMBER_INFO_UPDATED" then
+		print(event);
+		
+	end;
+
+	if event == "ZONE_CHANGED" then
+		--Runs once to initialize data after loading is done
+		ns.keyID = C_MythicPlus.GetOwnedKeystoneChallengeMapID();
+		if ns.keyID ~= nil then 
+			ns.key = C_ChallengeMode.GetMapUIInfo(ns.keyID);
+			ns.level = format("%s", C_MythicPlus.GetOwnedKeystoneLevel());
+			--update the table and send data out (true flag)
+			ns:UpdateKey(ns.player, ns.key, ns.level, date("%Y %m %d %H:%M"), true);			
+		end;	
+		ns:SendKeys();
+		ns:SendData("refresh");
+		refreshFlag = false;
+		frame:UnregisterEvent("ZONE_CHANGED");
+	end;
+		
+	if event == "PLAYER_ENTERING_WORLD" then 
+		local icon = LibStub("LibDBIcon-1.0", true);
+		if not KeyKeeperLDBIconDB then KeyKeeperLDBIconDB = {} end;
+		icon:Register("KeyKeeper", kkLDB, KeyKeeperLDBIconDB)
+		if GetRealmName() == "Earthen Ring" then
+			if ns:GetSaveResetTime() < time() then
+				--if we have passed the reset time then reset
+				ns:ResetDataFile();
+			end;
+			--ns:GetSaveResetTime() will ensure KeyKeeper["settings"] is valid
+			if KeyKeeper["settings"]["debug"] == nil then KeyKeeper["settings"]["debug"] = false; end;
+			ns.debug = KeyKeeper["settings"]["debug"];
+			if ns.debug then print("KeyKeeper: Debug mode active."); end;
+			--only allow this to run the first time then stop monitoring
+			frame:UnregisterEvent("PLAYER_ENTERING_WORLD");
+		else
+			--Don't monitor events if your not on Earthen Ring
+			frame:UnregisterEvent("ZONE_CHANGED");
+			frame:UnregisterEvent("BAG_UPDATE");
+			frame:UnregisterEvent("PLAYER_ENTERING_WORLD");
+			frame:UnregisterEvent("CHAT_MSG_ADDON");
+		end;
+	end;
+	
+	if event == "BAG_UPDATE" then
+		--check key, if different update local data
+		local id = C_MythicPlus.GetOwnedKeystoneChallengeMapID();
+		if id ~= nil then
+			local lvl = format("%s", C_MythicPlus.GetOwnedKeystoneLevel());
+			if ns.debug and IsInInstance() then print("BAG_UPDATE: lvl = ",lvl, " id = ", id ); end;
+			if ((ns.keyID ~= id) or (ns.level ~= lvl)) then 
+				if ns.debug then print("Old Key ", ns.keyID, ns.level, " - New Key ", id, lvl); end;
+				ns.keyID = id;
+				ns.key = C_ChallengeMode.GetMapUIInfo(ns.keyID);
+				ns.level = lvl;
+				--update the table and send data out (true flag)
+				if ns.debug then print("Updating Keystone."); end;
+				ns:UpdateKey(ns.player, ns.key, ns.level, date("%Y %m %d %H:%M"), true);
+				local msg = "New keystone, " .. ns.level .. " - " .. ns.key;
+				if UnitInParty("player") then
+					SendChatMessage(msg ,"PARTY");
+				else
+					print(msg);
+				end;
+			end;
+		end;
+	end; 
+
+	if event == "CHAT_MSG_ADDON" then
+		--fires when an addon message is received
+		local Addon, Message, Stream, Source,_,_,_, ChannelName  = ...; 
+		--watching for KeyKeeper
+		if Addon ~= ns.prefix then return;	end;	
+		--Ignore messages from ourself
+		local n = strsplit("-", Source);
+		if n ~= ns.player then
+			if Message == "refresh" then
+				--another user requested a refresh, send your data
+				if ns.debug then print(n, " requested a refresh."); end;
+				ns:SendKeys();						
+			else
+				--another user sent data, just update our data 
+				--false flag prevents sending out again
+				--parse out variables from message
+				local toon, stone, level, dt, ver = strsplit( "#", Message );
+				if ns.debug then print(n, " sent ", Message); end;
+				ns:UpdateKey(toon, stone, level, dt, false);	
+			end;
+			if refreshFlag then
+				ns:SendData("refresh");
+				refreshFlag = false;
+			end;			
+		end;
+	end;
+
 	if event == "CHAT_MSG_GUILD" or event == "CHAT_MSG_OFFICER" or event == "CHAT_MSG_PARTY" or event == "CHAT_MSG_WHISPER" then
 		local msg, player = ...; 
 		if strfind(msg, "Keystone:") and player == "Tem-EarthenRing" then
@@ -128,95 +230,8 @@ function frame:OnEvent(event, ...)
 		end;
 	end;
 
-	if event == "ZONE_CHANGED" then
-		--Runs once to initialize data after loading is done
-		ns.keyID = C_MythicPlus.GetOwnedKeystoneChallengeMapID();
-		if ns.keyID ~= nil then 
-			ns.key = C_ChallengeMode.GetMapUIInfo(ns.keyID);
-			ns.level = format("%s", C_MythicPlus.GetOwnedKeystoneLevel());
-			--update the table and send data out (true flag)
-			ns:UpdateKey(ns.player, ns.key, ns.level, date("%Y %m %d %H:%M"), true);			
-		end;	
-		ns:SendKeys();
-		ns:SendData("refresh");
-		refreshFlag = false;
-		frame:UnregisterEvent("ZONE_CHANGED");
-	end;
+
 	
-	if event == "BAG_UPDATE" then
-		--check key, if different update local data
-		local id = C_MythicPlus.GetOwnedKeystoneChallengeMapID();
-		if id ~= nil then
-			local lvl = format("%s", C_MythicPlus.GetOwnedKeystoneLevel());
-			if ns.debug and IsInInstance() then print("BAG_UPDATE: lvl = ",lvl, " id = ", id ); end;
-			if ((ns.keyID ~= id) or (ns.level ~= lvl)) then 
-				if ns.debug then print("Old Key ", ns.keyID, ns.level, " - New Key ", id, lvl); end;
-				ns.keyID = id;
-				ns.key = C_ChallengeMode.GetMapUIInfo(ns.keyID);
-				ns.level = lvl;
-				--update the table and send data out (true flag)
-				if ns.debug then print("Updating Keystone."); end;
-				ns:UpdateKey(ns.player, ns.key, ns.level, date("%Y %m %d %H:%M"), true);
-				local msg = "New keystone, " .. ns.level .. " - " .. ns.key;
-				if UnitInParty("player") then
-					SendChatMessage(msg ,"PARTY");
-				else
-					print(msg);
-				end;
-			end;
-		end;
-	end;
-	
-	if event == "PLAYER_ENTERING_WORLD" then 
-		local icon = LibStub("LibDBIcon-1.0", true);
-		if not KeyKeeperLDBIconDB then KeyKeeperLDBIconDB = {} end;
-		icon:Register("KeyKeeper", kkLDB, KeyKeeperLDBIconDB)
-		if GetRealmName() == "Earthen Ring" then
-			if ns:GetSaveResetTime() < time() then
-				--if we have passed the reset time then reset
-				ns:ResetDataFile();
-			end;
-			--ns:GetSaveResetTime() will ensure KeyKeeper["settings"] is valid
-			if KeyKeeper["settings"]["debug"] == nil then KeyKeeper["settings"]["debug"] = false; end;
-			ns.debug = KeyKeeper["settings"]["debug"];
-			if ns.debug then print("KeyKeeper: Debug mode active."); end;
-			--only allow this to run the first time then stop monitoring
-			frame:UnregisterEvent("PLAYER_ENTERING_WORLD");
-		else
-			--Don't monitor events if your not on Earthen Ring
-			frame:UnregisterEvent("ZONE_CHANGED");
-			frame:UnregisterEvent("BAG_UPDATE");
-			frame:UnregisterEvent("PLAYER_ENTERING_WORLD");
-			frame:UnregisterEvent("CHAT_MSG_ADDON");
-		end;
-	end; 
-	
-	if event == "CHAT_MSG_ADDON" then
-		--fires when an addon message is received
-		local Addon, Message, Stream, Source,_,_,_, ChannelName  = ...; 
-		--watching for KeyKeeper
-		if Addon ~= ns.prefix then return;	end;	
-		--Ignore messages from ourself
-		local n = strsplit("-", Source);
-		if n ~= ns.player then
-			if Message == "refresh" then
-				--another user requested a refresh, send your data
-				if ns.debug then print(n, " requested a refresh."); end;
-				ns:SendKeys();						
-			else
-				--another user sent data, just update our data 
-				--false flag prevents sending out again
-				--parse out variables from message
-				local toon, stone, level, dt, ver = strsplit( "#", Message );
-				if ns.debug then print(n, " sent ", Message); end;
-				ns:UpdateKey(toon, stone, level, dt, false);	
-			end;
-			if refreshFlag then
-				ns:SendData("refresh");
-				refreshFlag = false;
-			end;			
-		end;
-	end;
 	
 end	
 frame:SetScript("OnEvent", frame.OnEvent); 
